80977a1dec5cdbd99f5110e0cbe32a30e0d4cca9
James Steven Jenkins <sjenkins@studioj.us>
2025-07-16 16:20:39 -0400

    runs native binary (on $PATH)

--- lib/concorde.rb
+++ lib/concorde.rb
@@ -22,24 +22,22 @@ class Concorde
 
       # Create 'tsplib' directory in temporary directory.
 
-      tspdir = "#{dir}/tsplib"
-      Dir.mkdir(tspdir)
+      Dir.chdir(dir) do
 
-      # Write TSP file into 'tsplib' directory.
+       File.open("problem.tsp", 'w') do |f|
+          f.puts(@tsp.to_s)
+        end
 
-      File.open("#{tspdir}/problem.tsp", 'w') do |f|
-        f.puts(@tsp.to_s)
+        concorde_cmd = "concorde -o problem.sol problem.tsp 2> /dev/null"
+        o, e, s = Open3.capture3(concorde_cmd)
+
+        @cost = o.match(/Optimal Solution:\s+([[:digit:]]+)(\.[[:digit:]]+)?/)[1].to_i
+
+        @tour = File.open("problem.sol") do |f|
+          f.read.split.drop(1).map { |s| s.to_i }
+        end
+
       end
-
-      docker_cmd = "docker run --rm -t -v #{tspdir}:/usr/local/opt/concorde/ alehkot/concorde-tsp:1.0 problem.tsp 2> /dev/null"
-      o, e, s = Open3.capture3(docker_cmd)
-
-      @cost = o.match(/Optimal Solution:\s+([[:digit:]]+)(\.[[:digit:]]+)?/)[1].to_i
-
-      @tour = File.open("#{tspdir}/problem.sol") do |f|
-        f.read.split.drop(1).map { |s| s.to_i }
-      end
-
     end
   end
 
